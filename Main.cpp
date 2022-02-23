2e8f34bb9ac25f1b1f27c9fb24f90da1e41ad3f9
Sturm0 <89245373+Sturm0@users.noreply.github.com>
2022-02-23 06:00:49 -0300

    Update Main

--- Main.cpp
+++ Main.cpp
@@ -4,27 +4,43 @@
 #include <stdlib.h> //esto es necesario para obtener números aleatorios y también para limpiar la pantalla
 #include <ctime>
 
-
-//definitivamente no tengo idea como funciona esto de define y demás, lo debería averiguar
-/*#ifdef __unix__         
-
-#elif defined(_WIN32) || defined(WIN32) 
-
-#define OS_Windows
-
-#endif*/
-
+std::string getOsName()
+{
+    #ifdef _WIN32 || _WIN64
+    return "Windows";
+    #elif __linux__ || __unix
+    return "Linux";
+    #else
+    return "Other";
+    #endif
+} 
 
 void limpiar_pantalla(){
 	//esta función no parece estar funcionando del todo bien
-	//#ifdef OS_Windows
-	 /* Windows code */
-	//system("CLS");
-	//#else
-	 /* GNU/Linux code */
-	system("clear");
-	//#endif    
+	if (getOsName == "Windows"){
+		system("CLS");
+	}
+	else {
+
+		system("clear");
+	}
+	
 }
+void cambiar_asignacion(bool VF){
+	//GNU LINUX CODE <-- hacer que esta sección se ejecute dependiendo del SO
+	if (getOsName == "Linux"){
+		if(VF)
+		{
+			system("xmodmap -e \"keycode 36 = space\" && xmodmap -e \"keycode 65 = Return\"");
+		} 
+		else 
+		{
+			system("xmodmap -e \"keycode 65 = space\" && xmodmap -e \"keycode 36 = Return\"");
+		}	
+	}
+	
+}
+
 void mostrar_lista(std::string conjunto[],char estado[],unsigned short &mostrar, const int valor = 0){
 	for (int i = mostrar-25; i <= mostrar ;i++){
 		switch(estado[i]){
@@ -43,13 +59,15 @@ void mostrar_lista(std::string conjunto[],char estado[],unsigned short &mostrar,
 }
 
 int main(){
-	std::string array[200] = {"de", "Y", "el", "la", "en", "a", "que", "los", "se", "que", "un", "las", "con", "no", "por", "una", "para", "su", "es", "como", "me", "más", "le", "lo", "o", "pero", "sus", "si", "este", "sobre", "entre", "cuando", "también", "todo", "era", "fue", "esta", "ya", "son", "mi", "sin", "la", "años", "ser", "nos", "te", "qué", "dos", "está", "muy", "desde", "porque", "yo", "hasta", "había", "hay", "tiene", "ese", "todos", "hacer", "donde", "eso", "puede", "parte", "vida", "uno", "esa", "tiempo", "él", "ella", "sólo", "dijo", "cada", "vez", "ni", "otro", "después", "otros", "mismo", "hace", "ahora", "les", "estaba", "así", "bien", "e", "día", "año", "aunque", "durante", "país", "siempre", "otra", "tres", "algo", "ver", "mundo", "los", "tan", "antes", "sí", "cómo", "casa", "nada", "trabajo", "estos", "momento", "quien", "están", "gran", "esto", "forma", "mayor", "personas", "ellos", "nacional", "gobierno", "sino", "primera", "unos", "hacia", "tenía", "entonces", "hoy", "lugar", "ante", "luego", "estado", "otras", "días", "tener", "pues", "va", "contra", "nunca", "casi", "tienen", "según", "algunos", "una", "manera", "nuevo", "además", "hombre", "millones", "dar", "mucho", "veces", "menos", "todas", "primer", "presidente", "decir", "mujer", "tu", "solo", "mientras", "cosas", "mí", "debe", "tanto", "aquí", "estas", "ciudad", "fueron", "historia", "más", "sin embargo", "toda", "tras", "pueden", "dice", "tipo", "las", "grupo", "cual", "social", "gente", "sistema", "desarrollo", "mejor", "noche", "misma", "estar", "lado", "muchos", "sea", "cuenta", "mujeres", "agua", "importante", "aún", "dentro", "cuatro", "información", "mis", "madre", "salud", "nuestro", "será"};
+	const std::string array[200] = {"de", "Y", "el", "la", "en", "a", "que", "los", "se", "que", "un", "las", "con", "no", "por", "una", "para", "su", "es", "como", "me", "más", "le", "lo", "o", "pero", "sus", "si", "este", "sobre", "entre", "cuando", "también", "todo", "era", "fue", "esta", "ya", "son", "mi", "sin", "la", "años", "ser", "nos", "te", "qué", "dos", "está", "muy", "desde", "porque", "yo", "hasta", "había", "hay", "tiene", "ese", "todos", "hacer", "donde", "eso", "puede", "parte", "vida", "uno", "esa", "tiempo", "él", "ella", "sólo", "dijo", "cada", "vez", "ni", "otro", "después", "otros", "mismo", "hace", "ahora", "les", "estaba", "así", "bien", "e", "día", "año", "aunque", "durante", "país", "siempre", "otra", "tres", "algo", "ver", "mundo", "los", "tan", "antes", "sí", "cómo", "casa", "nada", "trabajo", "estos", "momento", "quien", "están", "gran", "esto", "forma", "mayor", "personas", "ellos", "nacional", "gobierno", "sino", "primera", "unos", "hacia", "tenía", "entonces", "hoy", "lugar", "ante", "luego", "estado", "otras", "días", "tener", "pues", "va", "contra", "nunca", "casi", "tienen", "según", "algunos", "una", "manera", "nuevo", "además", "hombre", "millones", "dar", "mucho", "veces", "menos", "todas", "primer", "presidente", "decir", "mujer", "tu", "solo", "mientras", "cosas", "mí", "debe", "tanto", "aquí", "estas", "ciudad", "fueron", "historia", "más", "sin embargo", "toda", "tras", "pueden", "dice", "tipo", "las", "grupo", "cual", "social", "gente", "sistema", "desarrollo", "mejor", "noche", "misma", "estar", "lado", "muchos", "sea", "cuenta", "mujeres", "agua", "importante", "aún", "dentro", "cuatro", "información", "mis", "madre", "salud", "nuestro", "será"};
 	std::string array_desordenado[200];
 	char estado[200]; //contiene el estado del array desordenado, esto determina en que color se va a mostrar
 	std::string respuesta;
 	int tiempo_inicial = time(NULL);
 	int tiempo_limite = tiempo_inicial+60;
 	unsigned short mostrar = 25; //esta variable es usada para decidir que palabras mostrar y cuales no
+
+	cambiar_asignacion(true);
 
 	for (int i = 0; i < 200;i++){
 		estado[i] = 'N';
@@ -74,9 +92,14 @@ int main(){
 		std::cout << "\nPalabra: ";
 		std::cin >> respuesta;
 		
-		if (respuesta == array_desordenado[i])
-			{ estado[i] = 'T';
-		} else {
+		if (respuesta == array_desordenado[i]){ 
+			estado[i] = 'T';
+		} 
+		else if (respuesta == "salir" or respuesta == "exit"){
+			cambiar_asignacion(false);
+			exit(0);
+		}
+		else {
 			estado[i] = 'F';
 		}
 		limpiar_pantalla();
@@ -88,7 +111,7 @@ int main(){
 	for (int i = 0; i<200;i++){
 		if (estado[i] == 'T') palabras_correctas += 1; 
 	}
-
+	cambiar_asignacion(false);
 	std::cout << "\nPalabras por minuto: " << palabras_correctas << "\n";
 	
 }
